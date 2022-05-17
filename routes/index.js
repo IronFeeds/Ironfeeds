b2003d1f7b9b2122f7e418943aa17e3e91051f37
Samy Ali <samy.alisanchez91@gmail.com>
2022-05-17 15:10:00 +0200

    pagination done

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,50 @@ const API =
 const Article = require("../models/Article.model");
 const User = require("../models/User.model");
 const Category = require("../models/Category.model");
+const { count } = require("../models/Article.model");
 
 //Save an article
 
-router.get("/", (req, res) =>
+router.get("/", (req, res) => {
+  
+  /////////////
+  Article.countDocuments()
+  .then((count)=>{
+      //pagination////////
+      let { page = 1, perPage = 10 } = req.query;
+      page = parseInt(page);
+      perPage = parseInt(perPage);
+    
+      const limit = perPage;
+      const skip = (page - 1) * perPage;
+    
+      const totPages = Math.ceil(count/perPage)
+
+      //prevpage
+      let prevPage = page-1;
+      if(prevPage <= 0) prevPage = 1;
+    
+      //nextpage
+      let nextPage = page + 1;
+      if(nextPage >= totPages ) nextPage = totPages;
+
   Article.find()
     .populate("category")
-    .then((articles) => res.render("index", { name: articles }))
-);
+    .limit(limit)
+    .skip(skip)
+    .then((articles) =>{
+      res.render("index", {
+        name: articles,
+        page,
+        prevPage,
+        nextPage,
+        perPage,
+        totPages
+      })}
+    )
+})
+.catch((err) => console.log(err));
+});
 
 router.post("/user/save/:articleid", (req, res) => {
   const user = req.session.currentUser;
@@ -26,15 +62,33 @@ router.post("/user/save/:articleid", (req, res) => {
         );
         console.log("nobody");
       } else {
-        User.findByIdAndUpdate(
-          user._id,
-          { $pull: { savedArticles: {$in: articleid } }}
-        );
+        User.findByIdAndUpdate(user._id, {
+          $pull: { savedArticles: { $in: articleid } },
+        });
         console.log("user", user);
       }
     }
   );
   /*      .then(()=> res.redirect(`/#${articleid}`))  */
+});
+
+//Pagination
+router.get("/", async (req, res) => {
+  try {
+    let { page = 1, perPage = 10 } = req.query;
+    page = parseInt(page);
+    perPage = parseInt(perPage);
+
+    const limit = perPage;
+    const skip = (page - 1) * perPage;
+
+    /*  const users = await User.find({}, {}, {limit: limit, skip: skip}); */
+    const users = await User.find().limit(limit).skip(skip);
+    console.log("holaaaaaaaaaaaaaaa", users);
+    res.render("index", users);
+  } catch {
+    (err) => console.log(err);
+  }
 });
 
 module.exports = router;
